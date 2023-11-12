5f785ba23914f6c262629a595ce3fd9ff5c46af2
Lukasz <lukasz,kocurek@edu.uekat.pl>
2023-11-12 16:40:13 +0100

    uploading all to repo

---
+++ src/test/java/com/KociDev/contentCalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KociDev.contentCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

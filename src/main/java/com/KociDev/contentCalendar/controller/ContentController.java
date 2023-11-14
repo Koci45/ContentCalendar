ac04b32a2aef562e0d15c7224e5bf1759bdfaf7a
Lukasz <lukasz,kocurek@edu.uekat.pl>
2023-11-15 00:35:06 +0100

    Added ContentController class and ContentControllerRepository class

--- src/main/java/com/KociDev/contentCalendar/controller/ContentController.java
+++ src/main/java/com/KociDev/contentCalendar/controller/ContentController.java
@@ -1,9 +1,25 @@
 package com.KociDev.contentCalendar.controller;
 
+import com.KociDev.contentCalendar.model.Content;
+import com.KociDev.contentCalendar.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    //make a request and find all the content in system
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
+    }
 }
--- src/main/java/com/KociDev/contentCalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/KociDev/contentCalendar/repository/ContentCollectionRepository.java
@@ -10,12 +10,15 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
     private final List<Content> content = new ArrayList<>();
+
     public ContentCollectionRepository(){
 
     }
+
     public List<Content> findAll(){
         return content;
     }
+
     public Optional<Content> findById(Integer id){
         return content.stream().filter(c -> c.id().equals(id)).findFirst();
     }

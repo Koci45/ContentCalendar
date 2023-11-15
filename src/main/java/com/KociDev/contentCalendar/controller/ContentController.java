9ff1e172b793e7111aae7bd95331fb0523425007
Lukasz <lukasz,kocurek@edu.uekat.pl>
2023-11-15 19:43:34 +0100

    added post request controller for content

--- src/main/java/com/KociDev/contentCalendar/controller/ContentController.java
+++ src/main/java/com/KociDev/contentCalendar/controller/ContentController.java
@@ -2,11 +2,12 @@ package com.KociDev.contentCalendar.controller;
 
 import com.KociDev.contentCalendar.model.Content;
 import com.KociDev.contentCalendar.repository.ContentCollectionRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
@@ -21,5 +22,15 @@ public class ContentController {
     @GetMapping("")
     public List<Content> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
+    }
+
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
     }
 }
--- src/main/java/com/KociDev/contentCalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/KociDev/contentCalendar/repository/ContentCollectionRepository.java
@@ -1,25 +1,46 @@
 package com.KociDev.contentCalendar.repository;
 
 import com.KociDev.contentCalendar.model.Content;
+import com.KociDev.contentCalendar.model.Status;
+import com.KociDev.contentCalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository(){
 
     }
 
     public List<Content> findAll(){
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.add(content);
+    }
+
+    @PostConstruct
+    private void init(){
+        Content content = new Content(1,
+                "My first blog post",
+                "My first blog post",
+                Status.IDEA, Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        contentList.add(content);
     }
 }
